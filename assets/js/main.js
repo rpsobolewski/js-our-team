2279b4a9ea46aec5d7e1d15c724d218ac14831a1
rpsobolewski <rpsobolewski@gmail.com>
2023-09-06 15:46:38 +0200

    object array

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede */
+
+const team = [
+    {
+        name : 'Wayne Barnett',
+        position : 'Founder & CEO',
+        photo : "wayne-barnett-founder-ceo.jpg" ,
+    },
+    {
+        name : 'Angela Caroll',
+        position : 'Chief Editor',
+        photo : "angela-caroll-chief-editor.jpg" ,
+    },
+    {
+        name : 'Walter Gordon',
+        position : 'Office Manager',
+        photo : "walter-gordon-office-manager.jpg" ,
+    },
+    {
+        name : 'Angela Lopez',
+        position : 'Social Media Manager',
+        photo : "angela-lopez-social-media-manager.jpg" ,
+    },
+    {
+        name : 'Scott Estrada',
+        position : 'Developer',
+        photo : "scott-estrada-developer.jpg" ,
+    },
+    {
+        name : 'Barbara Ramos',
+        position : 'Graphic Designer',
+        photo : "barbara-ramos-graphic-designer.jpg" ,
+    },
+
+];
