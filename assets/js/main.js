6e52bdb6d94cb845e740c965ff47659f7ce5203a
rpsobolewski <rpsobolewski@gmail.com>
2023-09-06 17:27:12 +0200

    function
    transformed the "add new member" into a function

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,38 +13,51 @@ Organizzare i singoli membri in card/schede */
 
 
 
+function addNewMember(member) {
+    const markup =
+        `<div class="col-4 mb-5">
+        <div class="card">
+            <img src="${member.photo}">
+            <div class="card-body">
+                <h4>${member.name}</h4>
+                <p> ${member.position}</p>
+            </div>
+        </div>
+    </div>`
 
+    roeElement.insertAdjacentHTML('beforeend', markup);
+}
 
 const team = [
     {
         name: 'Wayne Barnett',
         position: 'Founder & CEO',
-        photo: "wayne-barnett-founder-ceo.jpg",
+        photo: "./assets/img/wayne-barnett-founder-ceo.jpg",
     },
     {
         name: 'Angela Caroll',
         position: 'Chief Editor',
-        photo: "angela-caroll-chief-editor.jpg",
+        photo: "./assets/img/angela-caroll-chief-editor.jpg",
     },
     {
         name: 'Walter Gordon',
         position: 'Office Manager',
-        photo: "walter-gordon-office-manager.jpg",
+        photo: "./assets/img/walter-gordon-office-manager.jpg",
     },
     {
         name: 'Angela Lopez',
         position: 'Social Media Manager',
-        photo: "angela-lopez-social-media-manager.jpg",
+        photo: "./assets/img/angela-lopez-social-media-manager.jpg",
     },
     {
         name: 'Scott Estrada',
         position: 'Developer',
-        photo: "scott-estrada-developer.jpg",
+        photo: "./assets/img/scott-estrada-developer.jpg",
     },
     {
         name: 'Barbara Ramos',
         position: 'Graphic Designer',
-        photo: "barbara-ramos-graphic-designer.jpg",
+        photo: "./assets/img/barbara-ramos-graphic-designer.jpg",
     },
 
 ];
@@ -69,18 +82,7 @@ for (let i = 0; i < team.length; i++) {
     console.log(member.position);
     console.log(member.photo);
 
-    const markup =
-        `<div class="col-4 mb-5">
-        <div class="card">
-            <img src="./assets/img/${member.photo}">
-            <div class="card-body">
-                <h4>${member.name}</h4>
-                <p> ${member.position}</p>
-            </div>
-        </div>
-    </div>`
-
-    roeElement.insertAdjacentHTML('beforeend', markup);
+    addNewMember (member)
 
 
 }
@@ -111,18 +113,7 @@ document.getElementById('button').addEventListener('click', function (e) {
         console.log(member.position);
         console.log(member.photo);
     
-        const markup =
-            `<div class="col-4 mb-5">
-            <div class="card">
-                <img src="${member.photo}">
-                <div class="card-body">
-                    <h4>${member.name}</h4>
-                    <p> ${member.position}</p>
-                </div>
-            </div>
-        </div>`
-    
-        roeElement.insertAdjacentHTML('beforeend', markup);
+        addNewMember(member)
     
     
     }
